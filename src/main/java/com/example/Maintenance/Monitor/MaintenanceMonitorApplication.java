faad10891a3ec428864b2b231e6f530d53059856
Name <email>
2023-07-01 10:24:24 +0200

    Initial commit

---
+++ src/main/java/com/example/Maintenance/Monitor/MaintenanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Maintenance.Monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Maintenance/Monitor/MaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Maintenance.Monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
